48a4efd0162e452a51badae2509d04d531a880c6
Anton Chaus <anchaus@c1r3p4.khpi.ucode-connect.study>
2020-11-20 17:07:48 +0200

    fix some funcs

--- sprints/sprint03/t05/mx_strcmp.c
+++ sprints/sprint03/t05/mx_strcmp.c
@@ -1,23 +1,10 @@
 int mx_strcmp(const char *s1, const char *s2) {
     int i = 0;
-    int pos = 0;
-    int neg = 0;
-    while(s1[i] != '\0' || s2[i] != '\0') {
-        if (s1[i] > s2[i]) {
-            pos++;
-        }
-        else if (s1[i] < s2[i]) {
-            neg -= 1;
-        }
-
+    while(s1[i] == s2[i]) {
+        if (s2[i] == '\0')
+            return 0;
         i++;
     }
-
-    if ((neg * -1) > pos) {
-        return neg;
-    }
-    else if (pos > (neg * -1)) {
-        return pos;
-    }
-    return 0;
+    return (s1[i] - s2[i]);
 }
+
